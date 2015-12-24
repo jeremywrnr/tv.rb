58ab6dbca9949e4adbc7a81cb6d63bb98b7975b1
Jeremy Warner <jeremywrnr@gmail.com>
2015-12-24 03:56:12 -0500

    clean colors

--- lib/tv.rb
+++ lib/tv.rb
@@ -12,28 +12,36 @@ end
 class TV
   def initialize(args)
     setup_colors
+    opts args
     resize
     parse args
   end
 
   private
+  def opts(args)
+    args.select { |arg| arg.chr == "-" }.map do |arg|
+      case arg
+      when "-v", "--version"
+        puts Version
+        exit 0
+      when "--wavy"
+        @wavy = true
+      when "--test"
+        @test = true
+      end
+    end
+  end
+
   def parse(args)
-    arg = args.shift
-    case arg
-    when "-v"
-      puts Version
-    when "--wave"
-      @wave = true
-    when "--test"
+    args = args.select { |arg| arg.chr != "-" }
+
+    if @test
       testing
-    when nil
-      helper
+
     else
-      set_bg args
+      set_bg args.first
       endless
     end
-
-    parse args unless args.empty?
   end
 
   def setup_colors
@@ -50,32 +58,28 @@ class TV
 
     # backgrounds
     @patts = {}
+    @patts['bw'] =    [blk, blk, wht, blk, wht, blk, blk, wht, wht]
+    @patts['xmas'] =  [grn, grn, red, red, grn, grn, red, red, red]
     @patts['error'] = [wht, gry, yel, cyn, grn, pnk, red, blu, blk]
     @patts['rasta'] = [blk, grn, grn, yel, yel, red, red, blk, blk]
     @patts['pride'] = [blk, ppl, blu, cyn, grn, yel, pnk, red, blk]
-    @patts['cloud'] = [blk, wht, blk, blk, wht, blk, wht, wht, blk]
 
     # pride for testing!
     @patt = @patts['pride']
     @colors = @patt.length
   end
 
-  def helper
-    print "Select a pattern (" << @patts.keys.join(", ") << "): "
-    initialize gets.chomp
-  end
-
   def resize
     @width = current_width
-    @colorwidth = (@width.to_f / @colors).floor
-    @offset = @wavy ? [-1, 0, 1, 1] : offset
+    @colorwidth = (@width / @colors).floor
+    @offset = @wavy? offset : [1]
   end
 
   def current_width
     TermInfo.screen_size[1]
   end
 
-  # make offset sample avg = offset difference. for example, if we have a final
+  # makes offset sample avg = offset difference. for example, if we have a final
   # gap of 3 characters, and a color width of 5, we would want to add 1 to the
   # width of three colors. on average, we can achieve this by sampling from an
   # array whose average is 3/5 - or [0,0,1,1,1].
@@ -85,8 +89,8 @@ class TV
   def offset
     off = []
 
-    ones = @colorwidth
-    zeros = (@width % @colorwidth) - ones
+    ones = @width % @colorwidth
+    zeros = @colorwidth - ones
 
     ones.times { |_| off << 1 }
     zeros.times { |_| off << 0 }
@@ -94,14 +98,10 @@ class TV
     off
   end
 
-  def average(arr)
-    arr.instance_eval { reduce(:+) / size.to_f }
-  end
-
   def tvline
     resize unless @width == current_width
 
-    @patt.each_with_index do |bg, oi|
+    @patt.each do |bg|
       space = " " * (@colorwidth + @offset.sample)
 
       print space.colorize bg
@@ -110,17 +110,21 @@ class TV
     sleep 0.01 # still tunable...
   end
 
-  def testing
-    100.times { |_| tvline }
+  def helper
+    print "Select a pattern (" << @patts.keys.join(", ") << "): "
+    parse [] << $stdin.gets.chomp
   end
 
   def set_bg(color)
     if @patts.keys.include? color
       @patt = @patts[color]
-
     else
       helper
     end
+  end
+
+  def testing
+    100.times { |_| tvline }
   end
 
   def endless
@@ -131,7 +135,9 @@ class TV
       while true do tvline end
     rescue Interrupt => e
       puts "\nThanks for watching!"
+      exit 0
     end
   end
 end
 
+    $stdout.flush
